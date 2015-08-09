4f664ffd88a5772ed5662eb88228bc8efc9d430f
sylwekpiorko <sylwesterpiorkowski@yahoo.ie>
2015-08-09 11:06:37 +0100

    Create probab.py
    main program

---
+++ probab.py
@@ -0,0 +1,26 @@
+import random
+
+dates = []
+for i in range (50):
+    a = int(random.random()*365)
+    dates.append(a)
+    
+#print dates
+dlugosc=len(dates)
+    
+datesSorted = sorted(dates)
+#print datesSorted
+
+
+dubel = 0
+
+for i in range(dlugosc-1):
+    pierwsza = datesSorted[i]
+    druga = datesSorted[i+1]
+    
+    if pierwsza == druga:
+        dubel += 1
+        break
+            
+
+print 'Jest dubelek ', pierwsza
